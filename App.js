d98659a31083502382655bd58796570cf5f8f6a7
IagoGoncalves <iago_375@hotmail.com>
2024-04-02 17:51:47 -0300

    initial files

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require("./src/assets/images/logo.png")}
+        style={styles.logo}
+      />
+      <Text>20 caracteres</Text>
     </View>
   );
 }
@@ -13,8 +15,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f3f3f3',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo:{
+    marginBottom: 60,
+  }
 });
